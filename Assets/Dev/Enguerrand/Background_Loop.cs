2f1e37d899d69bff089b8a02a2d807177a596013
riufix <enguerrand.cousin@gmail.com>
2023-09-08 10:15:16 +0200

    [Fix]: grayness

--- Assets/Dev/Enguerrand/Background_Loop.cs
+++ Assets/Dev/Enguerrand/Background_Loop.cs
@@ -7,7 +7,8 @@ public class Background_Loop : MonoBehaviour
     [SerializeField] Transform _pos1;
     [SerializeField] Transform _pos2;
     [SerializeField] AnimationCurve anim;
-    [SerializeField] float duration = 2.0f; 
+    [SerializeField] float duration = 3.0f;
+    [SerializeField] float backgroundSpeedupOverTime = 0.1f;
 
     // Update is called once per frame
     void Update()
@@ -16,5 +17,15 @@ public class Background_Loop : MonoBehaviour
         float t = animTime/duration;
         float t2 = anim.Evaluate(t);
         transform.position = Vector3.Lerp(_pos1.position, _pos2.position, t2);
+
+        if(duration > 1)
+        {
+            duration -= backgroundSpeedupOverTime * Time.deltaTime;
+            float animTime2 = Mathf.Repeat(Time.time, duration);
+            float t1 = animTime2 / duration;
+            float tbis = anim.Evaluate(t1);
+            transform.position = Vector3.Lerp(_pos1.position, _pos2.position, tbis);
+        }
+
     }
 }
